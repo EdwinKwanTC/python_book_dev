9a3c0074209401c430377d3749cb58ad59c50bf7
Edwin Kwan <edwinkwantc@gmail.com>
2023-08-20 22:49:13 +0800

    feat: adding word count function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+    word_count = 0
+    for word in file_contents.split():
+        chars = word.split()
+        for char in chars:
+            word_count += 1
+    print(word_count)
