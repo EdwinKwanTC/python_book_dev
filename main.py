fd9ec50a35b7cb8e9721423a8cd335839041563d
Edwin Kwan <edwinkwantc@gmail.com>
2023-08-23 23:02:54 +0800

    feat: completed chapter 16

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 with open("books/frankenstein.txt") as f:
+    result = {}
     file_contents = f.read()
     word_count = 0
-    for word in file_contents.split():
-        chars = word.split()
+    for words in file_contents.split():
+        chars = words.lower().split()[0]
         for char in chars:
+            if char in result:
+                result[char] += 1
+            else:
+                result[char] = 1
             word_count += 1
     print(word_count)
+    print(result)
